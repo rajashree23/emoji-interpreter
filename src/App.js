6f0b95949ed28dc325c5f2569798dd04a7884119
Rajashree Parhi <rajashree.parhi@factset.com>
2022-12-12 23:21:26 +0530

    feat: added emoji-interpreter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+export default function App() {
+  // var username = prompt("what name?");
+  // const [counter, setCounter] = useState(0);
+  // // var counter=0;
+  // function likeClickHandler() {
+  //   setCounter(counter + 1);
+  // }
+  const emojiDictionary = {
+    "😊": "smiling",
+    "😔": "sad",
+    "😯": "surprised",
+    "❤️": "love",
+    "😴": "sleepy",
+    "😕": "confused",
+    "😢": "crying",
+    "🍟": "french fries",
+    "🐕": "dog",
+    "🙃": "upside down",
+    "✨": "sparkles",
+    "⚽": "soccer ball"
+  };
+  var emojisWeKnow = Object.keys(emojiDictionary);
+  var [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    let meaning = emojiDictionary[event.target.value];
+    if (meaning === undefined) {
+      meaning = "Oops! We don't have meaning for the current emoji!";
+    }
+    setMeaning(meaning);
+  }
 
-function App() {
+  function emojiClickHandler(emoji) {
+    setMeaning(emojiDictionary[emoji]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>inside outtt</h1>
+
+      <input
+        placeholder="Put an emoji to understand the meaning!"
+        onChange={emojiInputHandler}
+      ></input>
+
+      <h2>{meaning}</h2>
+
+      <h3>Emojis we know</h3>
+
+      {emojisWeKnow.map((emoji) => {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            key={emoji}
+            style={{ cursor: "pointer", fontSize: "2rem", padding: "0.5rem" }}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
